927edcc6491584d9f810260bcf3d94e33797d728
DYDYICE <info@ice-mountain.com>
2025-07-10 13:58:02 +0200

    Create 20250710V1.js

---
+++ 20250710V1.js
@@ -0,0 +1,45 @@
+
+					//NUMMER + NAAM + 				CAT + 		LEVEL + CM//var
+var taal = "nl";
+//BUITEN
+let attractie1 =  ["1","Bunga Banga",			"attractie", "3", "90cm"];
+let attractie37 = ["37","NEW: Crazy Buggies",         "attractie", "3", "105cm"];
+let attractie2 =  ["2","Tam Tam twist",			"attractie", "3", "105cm"];
+let attractie3 =  ["3","Savana fall",			"attractie", "3", "90cm"];
+let attractie4 =  ["4","Flying Treehouse",		"attractie", "3", "90cm"];	
+let attractie5 =  ["5","Piratenboot ",			"attractie", "3", "90cm"];	
+let attractie6 =  ["6","Jungle Caroussel",		"attractie", "2", "90cm"];
+let attractie7 =  ["7","Jungle Express",		"attractie", "2", "80cm"];
+let attractie8 =  ["8","Tiki River",			"attractie", "2", "90cm"];
+let attractie9 =  ["9","Tribal Village",		"attractie", "3", "80cm"];	
+let attractie10 = ["10","Electric Mini Quads (â‚¬)",	"attractie", "3", "110cm"];
+let attractie11 = ["11","Opblaasbaar Parcours",	"attractie", "2", "80cm"];	
+let attractie12 = ["12","Trampolines",			"attractie", "2", "120cm"];	
+let attractie13 = ["13","Dinosaur Discovery",	"attractie", "1", "-"];	
+let attractie14 = ["14","Tropical Stage",		"show", 	 "2", "-"];	
+let attractie15 = ["15","Mini Trampolines",		"attractie", "1", "80cm"];
+let attractie16 = ["16","Mini Camp",			"attractie", "1", "80cm"];
+let attractie17 = ["17","Mini Speelkooi",		"attractie", "1", "80cm"];
+let attractie18 = ["18","mini Springkasteel",	"attractie", "1", "80cm"];
+let attractie19 = ["19","Loopwagentjes",		"attractie", "1", "80cm"];
+let attractie20 = ["20","Alligator Battle",         "attractie", "3", "90cm"];
+
+//BINNEN
+let attractie21 = ["32","Jungle Goolfy (â‚¬)",        "attractie", "3", "105cm"];
+let attractie22 = ["22","Big Fun Zone",         "attractie", "3", "90cm"];
+let attractie23 = ["23","Speelhall",            "attractie", "2", "50cm"];
+//let attractie24 = ["24","Elektrische Wagens(â‚¬)",   "attractie", "3", "110cm"];
+let attractie25 = ["25","Death Ride",           "attractie", "3", "90cm"];
+let attractie26 = ["26","Zone Lego",            "attractie", "1", "90cm"];
+let attractie27 = ["27","Speeltoren",           "attractie", "2", "90cm"];
+//let attractie28 = ["28","Spookhuis",            "attractie", "3", "90cm"];
+let attractie29 = ["29","CinÃ©ma 5D ",            "attractie", "3", "110cm"];
+let attractie30 = ["30","Foot & Basketbalzone", "attractie", "2", "90cm"];
+let attractie31 = ["31","Monkey Challenge",     "attractie", "2", "90cm"];
+let attractie32 = ["21","Baby Zone",            "attractie", "1", "50cm"];
+//HORECA
+let attractie33 = ["33","Jungle Restaurant",    "resto", "", "zomer/winter"];
+let attractie34 = ["34","Sunshine terrace",     "resto", "", "zomer/winter"];
+let attractie35 = ["35","Foodtruck /bar",       "resto", "", "zomer/winter"];
+//SHOP
+let attractie36 = ["36","Jungle Bazar",        "shop", "", "zomer/winter"];
